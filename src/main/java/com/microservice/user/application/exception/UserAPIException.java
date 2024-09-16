fd0c485ae078ab268417ec6fcf7b61bda6f6d917
Valentin <75315884+Contunder@users.noreply.github.com>
2024-09-17 00:01:31 +0200

    In Progress

---
+++ src/main/java/com/microservice/user/application/exception/UserAPIException.java
@@ -0,0 +1,22 @@
+package com.microservice.user.application.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+public class UserAPIException extends RuntimeException {
+
+    @Getter
+    private final HttpStatus status;
+    private final String message;
+
+    public UserAPIException(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+}
